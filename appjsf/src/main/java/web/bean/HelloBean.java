0137400357add6a3e37c17391dff91f3b6ddbd2f
andreluiz.breda <andreluiz.breda@SJ2161S-034.lab.infnet.com.br>
2020-03-07 16:24:55 -0300

    first commit

---
+++ appjsf/src/main/java/web/bean/HelloBean.java
@@ -0,0 +1,16 @@
+package web.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class HelloBean {
+
+	private String mensagem = "Ola jsf";
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+}
